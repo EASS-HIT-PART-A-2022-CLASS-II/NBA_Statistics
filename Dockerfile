8d2cf746e40d84cfc7849efda54f7d1a73a8285d
ron <ronpinto999@gmail.com>
2022-12-10 18:59:55 +0200

    nba2

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ WORKDIR /app
 RUN pip install -r requierments.txt
 EXPOSE  8000
 
-CMD ["uvicorn","main:app","--host=0.0.0.0","--reload"]
+CMD ["uvicorn","src.main:app","--host=0.0.0.0","--reload"]
 
--- src/main.py
+++ src/main.py
@@ -1,13 +1,43 @@
-import port as port
+
 from fastapi import FastAPI
 import uvicorn
+import http.client
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/get_all_players")
 def index():
-    return {"massage": "hello_world"}
+    conn = http.client.HTTPSConnection("free-nba.p.rapidapi.com")
+
+    headers = {
+        'X-RapidAPI-Key': "d76df82fddmsh6ea913d0e9198c3p180300jsn731409f18d41",
+        'X-RapidAPI-Host': "free-nba.p.rapidapi.com"
+        }
+
+    conn.request("GET", "/players?page=0&per_page=25", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+
+    return {data.decode("utf-8")} 
+
+
+@app.get("/get_one_player") 
+def index1():
+    conn = http.client.HTTPSConnection("free-nba.p.rapidapi.com")
+    headers = {
+    'X-RapidAPI-Key': "d76df82fddmsh6ea913d0e9198c3p180300jsn731409f18d41",
+    'X-RapidAPI-Host': "free-nba.p.rapidapi.com"
+    }
+    conn.request("GET", "/players/1", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    return(data.decode("utf-8"))
+
 
 
 if __name__ == "__main__":
