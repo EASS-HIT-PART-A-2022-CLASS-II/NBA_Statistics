43e3189e2b5c98f8926c31001645e5ebcea78f78
ron <ronpinto999@gmail.com>
2022-12-19 12:28:46 +0200

    update19

--- src/main.py
+++ src/main.py
@@ -2,12 +2,11 @@ import requests
 from .models import * 
 from fastapi import FastAPI
 import uvicorn
-import http.client
-import json
+
 
 app = FastAPI()
-@app.get("/get_all_players/{first_name}",response_model=all_data,response_model_exclude_unset=True)
-def index(first_name):
+@app.get("/get_player/{first_name}",response_model=all_data,response_model_exclude_unset=True)
+def get_player(first_name):
    url = "https://free-nba.p.rapidapi.com/players"
 
    querystring = {"page":"0","per_page":"25"}
@@ -23,47 +22,33 @@ def index(first_name):
     if i['first_name']==first_name:
         teams =all_data(
             id=i['id'],
+            
             first_name=i['first_name'],
             last_name=i['last_name'],
             position=i['position'],
-            error="ok",
+            error="no error",
             team=[
                 team(
                     id=i['team']['id'],
-                    abbreviation="",
+                    abbreviation=i['team']['abbreviation'],
                     city=i['team']['city'],
-                    conference="",
-                    division="",
-                    full_name="",
-                    name="",
-                    error="ok"
+                    conference=i['team']['conference'],
+                    division=i['team']['division'],
+                    full_name=i['team']['full_name'],
+                    name=i['team']['name'],
+                    error="no error"
                 )
             ]
         )
    return teams
 
 
-@app.get("/get_one_player") 
-def index1():
-    url = "https://free-nba.p.rapidapi.com/players/1"
-
-    headers = {
-        "X-RapidAPI-Key": "d76df82fddmsh6ea913d0e9198c3p180300jsn731409f18d41",
-        "X-RapidAPI-Host": "free-nba.p.rapidapi.com"
-    }
-
-    response = requests.request("GET", url, headers=headers)
-    response_dict=response.json()
-    
-
-
-    return (response_dict)
 
 
 
 
 @app.get("/get_all_teams") 
-def index2():
+def get_all_teams():
     url = "https://free-nba.p.rapidapi.com/teams"
 
     querystring = {"page":"0"}
@@ -85,8 +70,8 @@ def index2():
 
 
 
-@app.get("/get_teams_data/{name}",response_model=team,response_model_exclude_unset=True) 
-def index3(name):
+@app.get("/get_team/{name}",response_model=team,response_model_exclude_unset=True) 
+def get_team(name):
    url = "https://free-nba.p.rapidapi.com/teams"
    headers = {
         "X-RapidAPI-Key": "d76df82fddmsh6ea913d0e9198c3p180300jsn731409f18d41",
@@ -96,30 +81,21 @@ def index3(name):
    response_dict=response.json()
    for i in response_dict['data']:
             if i['name']==name:
+                team.name=i['name']
                 team.id=i['id']
                 team.city=i['city']
-                team.error="ok"
+                team.conference=i['conference']
+                team.full_name=i['full_name']
+                team.error="no error"
                 return team
    team.id=0000
    team.error="no data" 
    return team
 
-@app.get("/get_teams_data_test/{name}") 
-def indextst(name):
-   url = "https://free-nba.p.rapidapi.com/teams"
-   headers = {
-        "X-RapidAPI-Key": "d76df82fddmsh6ea913d0e9198c3p180300jsn731409f18d41",
-        "X-RapidAPI-Host": "free-nba.p.rapidapi.com"
-    }
-   response = requests.request("GET", url, headers=headers)
-   response_dict=response.json()
-   for i in response_dict['data']:
-    if i['name']==name:
-        return i
-   return (response_dict)
+
           
-@app.get("/get_teams_datas/{city}") 
-def index4(city):
+@app.get("/get_team_by_city/{city}",response_model=team,response_model_exclude_unset=True) 
+def get_team_city(city):
       url = "https://free-nba.p.rapidapi.com/teams"
 
       headers = {
@@ -133,33 +109,19 @@ def index4(city):
 
       for i in response_dict['data']:
                 if i['city']==city:
-                    return i 
+                    team.city=i['city']
+                    team.id=i['id']
+                    team.name=i['name']
+                    team.conference=i['conference']
+                    team.full_name=i['full_name']
+                    team.error="no error"
+                    return team 
       return (response_dict)
 
 
 
 
-@app.get("/test/{id}")
-def index6(id:int):
 
-    url = "https://free-nba.p.rapidapi.com/players"
-
-    querystring = {"page":"0","per_page":"25"}
-
-    headers = {
-        "X-RapidAPI-Key": "d76df82fddmsh6ea913d0e9198c3p180300jsn731409f18d41",
-        "X-RapidAPI-Host": "free-nba.p.rapidapi.com"
-    }
-    
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-
-    response_dict=response.json()
-
-    for i in response_dict['data']:
-        if i['id']==id:
-            return i 
-    return (response_dict)
 
 
 
--- src/test_main.py
+++ src/test_main.py
@@ -1,6 +1,28 @@
 from .main import *
 
-def test_data():
-    response = index("Ron")
-    assert response.error=="ok"
+
+def test_get_player():
+    response = get_player("Ron")
+    assert response.error=="no error"
     assert response.first_name=="Ron"
+
+
+
+def test_get_player_false():
+    response = get_player("Ron")
+    assert response.error=="no error"
+    assert response.first_name=="Sahar"
+
+
+def test_get_team():
+    response = get_team("Lakers")
+    assert response.error=="no error"
+    assert response.name=="Lakers"
+
+def test_get_city():
+    response = get_team_city("LA")
+    assert response.city=="LA"
+
+def test_get_teams():
+    response = get_all_teams
+    assert response==response
