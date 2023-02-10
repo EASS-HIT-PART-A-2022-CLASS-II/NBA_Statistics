dcc0929a67fb36703ef750cb8c72bd1b6e0c3d23
ron <ronpinto999@gmail.com>
2023-02-10 18:11:50 +0200

    test 7

--- Frontend/Frontend.py
+++ Frontend/Frontend.py
@@ -3,7 +3,7 @@ import requests
 import json
 import pandas as pd 
 import pymongo
-client=pymongo.MongoClient("mongodb://ron_pinto:Aa123456@mongo:27017/icons")
+client=pymongo.MongoClient("mongodb://ron_pinto:Aa123456@db:27017/icons")
 db=client["icons_database"]
 collection=db["icons_collection"]
 
